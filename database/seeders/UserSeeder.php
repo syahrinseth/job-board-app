7a0b2487bd99365e11e5901c7386be11e19be4f3
syahrinseth <syahrinsethservices@gmail.com>
2025-11-04 21:44:51 +0800

    feat: add role field to User model and implement UserSeeder for database seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create super admin user
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
+        ]);
+
+        // Create some test users
+        User::factory(10)->create();
+    }
+}
